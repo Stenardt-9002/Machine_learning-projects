dfbd3a4923af8f44f665db65ff0e55cceaec5955
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2020-03-22 23:52:09 +0530

    Cpu Testing

--- activate.sh
+++ activate.sh
@@ -1,1 +1,15 @@
-source '/media/i7-workstation/New Volume/Sem6/PDP/code ml/v1/bin/activate'
+#!/usr/bin/env bash
+
+#activate () {
+# /media/i7-workstation/New Volume/Sem6/PDP/code ml/v1/bin/activate
+#/media/i7-workstation/New Volume/Sem6/PDP/code ml/v1/bin
+#}
+#alias activate="/media/i7-workstation/New Volume/Sem6/PDP/code ml/v1/bin/activate"
+#source activate
+
+source "/media/i7-workstation/New Volume/Sem6/PDP/code ml/v1/bin/activate"
+
+#"/media/i7-workstation/New Volume/Sem6/PDP/code ml/v1/bin/activate"
+
+
+#source "/media/i7-workstation/New Volume/Sem6/PDP/code ml/v1/bin/activate"
