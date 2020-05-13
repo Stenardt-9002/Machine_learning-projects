7d20d4130450dee4d36407878b40d713bce06d47
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2020-05-13 08:19:46 +0530

    Chatbot v1 error Tensorflow 1.3

--- AI-bot/main1.py
+++ AI-bot/main1.py
@@ -1,12 +1,12 @@
 import nltk 
 from nltk.stem.lancaster import LancasterStemmer
-import numpy 
+import numpy as np 
 import tflearn
 import tensorflow
 import os
 import random 
 import json 
-
+import pickle 
 
 
 stemmer = LancasterStemmer()
@@ -16,8 +16,18 @@ with open("data1.json") as file:
 
 # print(data)
 
+# try:
+#     with open("Bkupsave","rb") as f:
+#         words_lot,labels_lot,training_1,out_put = pickle.load(f)
+# except :
+#     pass
+
+
+
 words_lot = []
 labels_lot = []
+doc_x = []
+doc_y = []
 docs_lot = []
 
 for ine1 in data["intents"]:
@@ -25,8 +35,129 @@ for ine1 in data["intents"]:
         #stemming
         word_list = nltk.word_tokenize(patter)
         words_lot.extend(word_list)
+        # docs_lot.append(patter)
+        doc_x.append(word_list)
+        doc_y.append(ine1["tag"])
+    if ine1["tag"] not in labels_lot:
+        labels_lot.append(ine1["tag"])
 
-        pass
 
 
+# print(doc_x)      
+# print("\n\n\n\n")
+
+# print("words_lot = ",words_lot)
+# print("\n\n\n\n")
+
+# print("doc_x = ",doc_x)
+# print("\n\n\n\n")
+# print("doc_y = ",doc_y)
+# print("\n\n\n\n")
+
+# print("labels_lot = ",labels_lot)
+words = [stemmer.stem(w.lower()) for w in words_lot if w != "?"]
+# print(words)
+# print("\n\n\n\n")
+
+words = sorted(list(set(words)))
+# print("\n\n\n\n")
+# print("words = ",words)
+# print(words)
+# print("\n\n\n\n")
+
+
+labelsnew = sorted(labels_lot)
+# print("labelsnew = ",labelsnew)
+training_1 = []
+out_put = []
+
+
+out_put_empty = [0 for _ in range(len(labelsnew))]
+
+
+for x,doc in enumerate(doc_x):
+    bag = []
+
+    wrds = [stemmer.stem(wc) for wc in doc ]
+
+    for wc in words:
+        if wc in wrds:
+            bag.append(1)
+
+        else:
+            bag.append(0)
+
+    output_trow = out_put_empty[:]
+    output_trow[labelsnew.index(doc_y[x])] = 1        
+
+    training_1.append(bag)
+    out_put.append(output_trow)
+
+
+training_1 = np.array(training_1)
+out_put = np.array(out_put)
+
+
+
+tensorflow.reset_default_graph()
+
+net1 = tflearn.input_data(shape=[None,len(training_1[0])])
+
+net1 = tflearn.fully_connected(net1,8)
+net1 = tflearn.fully_connected(net1,8)
+net1 = tflearn.fully_connected(net1,len(out_put[0]),activation="softmax")
+net  = tflearn.regression(net1)
+
+mainmpdel = tflearn.DNN(net)
+
+# try:
+#     mainmpdel.load("Bkupsave")
+
+# except:
+#     mainmpdel.fit(training_1,out_put,n_epoch=1000,batch_size=8,show_metric=True)
+#     mainmpdel.save("Bkupsave")
+mainmpdel.fit(training_1,out_put,n_epoch=1000,batch_size=8,show_metric=True)
+mainmpdel.save("Bkupsave")
+
+def bag_of_words(s,words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(wc.lower()) for wc in s_words]
+
+    for sec in s_words:
+        for i,w in enumerate(words):
+            if w==sec:
+                bag[i] = 1
+                pass
+    return np.array(bag)
+
+
+
+def chat():
+    print("INitaite Sequence The bot (quit to exit)")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+            pass
+
+        resylt = mainmpdel.predict([bag_of_words(inp,words)])
+        # print(resylt)
+        # get max prob 
+        main_resylt_idex = np.argmax(resylt)
+        tag = labels_lot[main_resylt_idex]
+        # response = 
+        for tag1 in data["intents"]:
+            if tag1["tag"] == tag:
+                respomse = tag1["responses"]
+        print(respomse)
+
+        print(random.choice(respomse))
+
+
+
+print(out_put)
+ 
+chat()
 
--- Scratch_algo/svmsample.py
+++ Scratch_algo/svmsample.py
@@ -3,6 +3,7 @@ from matplotlib import style
 import numpy as np 
 style.use('ggplot')
 
+data_dict_val = {-1:np.array([[1,7],[2,8],[3,8]]) ,1:np.array([[5,1],[6,-1],[7,3]])}
 
 
 class SupportScatchr:
@@ -18,7 +19,7 @@ class SupportScatchr:
 
     def fit(self,data):
         self.data = data
-        opt_dict = {}
+        opt_dict = {} #mag of w : w and b
         transforms = [[1,1],
                         [-1,1],
                         [-1,-1],
@@ -29,16 +30,64 @@ class SupportScatchr:
         for yi in self.data:
             for featureset in self.data[yi]:
                 for feature in featureset:
-                    all_data.append()
+                    all_data.append(feature)
                     pass
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+        step_sizes = [self.max_feature_value *0.01]
+
+        b_range_multiple = 5 #not as precise 
+         
+        #
+        b_multiple = 5
+
+        latest_optimum = self.max_feature_value*10
+
+        for stp in step_sizes:
+            w = np.array([latest_optimum,latest_optimum])
+
+            optimized = False
+            while not optimized:
+                # min w and max b 
+                for b in np.arrange(-1*(self.max_feature_value*b_range_multiple) ,self.max_feature_value*b_range_multiple,stp*b_multiple):
+                    
+                    for transformation in transforms:
+                        w_t = w*transformation
+                        found_option1 = True 
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i 
+                                if not yi*(np.dot(w_t,xi)+b)>=1:
+                                    found_option1 = False 
+                            if found_option1:
+                                opt_dict[np.linalg.norm(w_t)] = [w_t,b]
+
+                        
+                        
+                if w[0]<0:
+                    optimized = True 
+                    print('optimised step')
+                else:
+                    w = w-stp
+                        
+
+            norms = sorted([n for n in opt_dict])
+            opt_choice = opt_dict[norms[0]]
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimum = opt_choice[0][0]+stp*2
+
+             
+
 
 
 
     def predict(self,features):
-        classi_fication = np.sign(np.dot(np.array(features),self.w)+self.b)
+        classi_fication = np.sign(np.dot(np.array(features),self.w)+self.b) #sign (x.w+b)
         
         return classi_fication
-        pass
+        
         
    
 
@@ -47,7 +96,6 @@ class SupportScatchr:
 
 
 
-data_dict_val = {-1:np.array([[1,7],[2,8],[3,8]]) ,1:np.array([[5,1],[6,-1],[7,3]])}
 
 
 
